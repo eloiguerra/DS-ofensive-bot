fbc8148b3c5ea6aba04d31d76865082a665ba8f1
eloiguerra <joseeloi.juninho@gmail.com>
2021-04-23 20:13:23 -0300

    toggle bot

--- src/index.js
+++ src/index.js
@@ -3,12 +3,18 @@ const Discord = require("discord.js");
 const fetch = require("node-fetch");
 const cliente = new Discord.Client();
 
+let active = true;
+
 cliente.on("ready", () => {
   console.log(`Bota no alto`);
 });
 
 cliente.on("message", (msg) => {
   if (msg.author.bot) return;
+
+  if (msg.content === "!pocas") active = !active;
+
+  if (!active) return;
 
   if (msg.content !== "") {
     fetch("http://xinga-me.appspot.com/api")
